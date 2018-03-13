3f6cd0950463b99664517a0806a5dfdc6a65f739
Rod Johnson <johnsonroda@gmail.com>
2018-03-13 17:28:23 +1100

    Everyone needs encouragement to write Java

--- src/main/java/atomist/Whitehall2Application.java
+++ src/main/java/atomist/Whitehall2Application.java
@@ -1,3 +1,5 @@
+// I come from the loving space of my heart, and I know that love opens all doors.
+
 // Deep at the center of my being is an infinite well of love.
 
 // It’s only a thought, and a thought can be changed.
